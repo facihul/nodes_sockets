5e0364942cdf5a68ded70e11cd2c0737dfae11bb
MD. Facihul Azam <facihul@yahoo.com>
2016-01-25 18:01:33 +0200

    added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -4,3 +4,7 @@ socket.on('connect', function (){
 
 	console.log('user connete via socekt.io .. ');
 }); 
+socket.on('message', function (message){
+  console.log('new message ');
+  console.log(message.text);
+});
