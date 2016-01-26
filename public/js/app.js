a8aac521456213dc82b99b1b36ded4e7b484f006
MD. Facihul Azam <facihul@yahoo.com>
2016-01-27 00:25:53 +0200

    chat room authentication

--- public/js/app.js
+++ public/js/app.js
@@ -3,9 +3,16 @@ var name = getQueryVariable('name') || 'Unknown';
 var socket = io();
 console.log(name + ' wants to join' + room);
 
+jQuery('.room-title').text(room);
+
 socket.on('connect', function() {
 
 	console.log('user connete via socekt.io .. ');
+	socket.emit('joinRoom', {
+		name: name,
+		room: room
+	});
+
 });
 
 socket.on('message', function (message) {
