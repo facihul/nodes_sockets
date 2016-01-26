be75c69fd3d7b6975fd1ecc7f2586f563a2bb49d
MD. Facihul Azam <facihul@yahoo.com>
2016-01-26 11:53:51 +0200

    creating front-end UI to submit message

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,25 @@
 var socket = io();
 
-socket.on('connect', function (){
+socket.on('connect', function() {
 
 	console.log('user connete via socekt.io .. ');
-}); 
-socket.on('message', function (message){
-  console.log('new message ');
-  console.log(message.text);
 });
+socket.on('message', function(message) {
+	console.log('new message ');
+	console.log(message.text);
+});
+
+var $form = jQuery('#message-form');
+$form.on('submit', function (event) {
+	event.preventDefault();
+	var $message = $form.find('input[name=message]');
+	socket.emit('message',{
+	 text: $message.val()
+     });
+	$message.val('');
+});
+
+
+
+
+
